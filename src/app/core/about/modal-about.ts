88153ac6587870101cb088fdd5bf1dc3ef1f967e
Benoit Lavenier <benoit.lavenier@e-is.pro>
2021-06-02 18:54:03 +0200

    [fix] About: use package.json for environement.issuerUrl and environement.sourceUrl [fix] Add missing classes in public_api.ts

--- src/app/core/about/modal-about.ts
+++ src/app/core/about/modal-about.ts
@@ -1,8 +1,10 @@
 import {Component, Inject, InjectionToken, Optional} from "@angular/core";
 import {ModalController} from "@ionic/angular";
 import {ENVIRONMENT} from "../../../environments/environment.class";
+import {capitalizeFirstLetter} from "../../shared/functions";
 import {Department} from "../services/model/department.model";
 import {Person} from "../services/model/person.model";
+import {TranslateService} from "@ngx-translate/core";
 
 export const APP_ABOUT_DEVELOPERS = new InjectionToken<Partial<Department>[]>('aboutDevelopers');
 export const APP_ABOUT_PARTNERS = new InjectionToken<Partial<Department>[]>('aboutPartners');
@@ -14,16 +16,23 @@ export const APP_ABOUT_PARTNERS = new InjectionToken<Partial<Department>[]>('abo
 })
 export class AboutModal {
 
+  readonly name: string;
   readonly version: string;
   readonly sourceUrl: string;
   readonly reportIssueUrl: string;
 
   constructor(
-      protected modalController: ModalController,
-      @Optional() @Inject(ENVIRONMENT) environment,
-      @Optional() @Inject(APP_ABOUT_DEVELOPERS) public developers: Partial<Department>[],
-      @Optional() @Inject(APP_ABOUT_PARTNERS) public partners: Partial<Department>[]
+    protected translate: TranslateService,
+    protected modalController: ModalController,
+    @Optional() @Inject(ENVIRONMENT) environment,
+    @Optional() @Inject(APP_ABOUT_DEVELOPERS) public developers: Partial<Department>[],
+    @Optional() @Inject(APP_ABOUT_PARTNERS) public partners: Partial<Department>[]
   ) {
+    this.name = translate.instant('APP_NAME');
+    if (this.name === 'APP_NAME') {
+      // Not translated: use name from the environement
+      this.name = environment && environment.name && capitalizeFirstLetter(environment.name) || undefined;
+    }
     this.version = environment && environment.version;
     this.sourceUrl = environment && environment.sourceUrl;
     this.reportIssueUrl = environment && environment.reportIssueUrl;
