4b63a6b69e56e24833e899b9d48d82bf017aaac4
Benoit Lavenier <benoit.lavenier@e-is.pro>
2021-06-08 15:01:26 +0200

    [fix] Fix AppTableUtils.waitIdle

--- src/app/core/table/table.utils.ts
+++ src/app/core/table/table.utils.ts
@@ -1,11 +1,10 @@
 import {AppTable} from "./table.class";
 import {debounceTime} from "rxjs/operators";
-import {Entity} from "../services/model/entity.model";
 import {firstFalsePromise} from "../../shared/observables";
 
 export class AppTableUtils {
 
-  static async waitIdle<T extends Entity<T> = Entity<any>, F = any>(table: AppTable<T, F>) {
+  static async waitIdle(table: AppTable<any, any, any>) {
 
     if (!table || !table.dataSource) {
       throw Error("Invalid table. Missing table or table.dataSource")
