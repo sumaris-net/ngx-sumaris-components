dd12940f7a59bcac717701a8695a91cfe5b5603f
Ludovic Pecquot <ludovic.pecquot@e-is.pro>
2021-06-10 16:35:38 +0200

    [fix] add EntityFilter.isCriteriaNotEmpty

--- src/app/core/services/model/filter.model.ts
+++ src/app/core/services/model/filter.model.ts
@@ -1,4 +1,4 @@
-import {isEmptyArray, isNotEmptyArray, isNotNilOrBlank} from '../../../shared/functions';
+import {isEmptyArray} from '../../../shared/functions';
 import {ReferentialUtils} from './referential.model';
 import {Entity, EntityAsObjectOptions, IEntity, isInstanceOf} from './entity.model';
 import {FilterFn} from '../../../shared/services/entity-service.class';
@@ -10,29 +10,28 @@ export const MINIFY_FOR_POD_OPTIONS = Object.freeze(<EntityAsObjectOptions>{
   keepLocalId: false
 });
 
-export interface IEntityFilter<
-  F extends IEntityFilter<F, T, ID, AO, FO>,
+export interface IEntityFilter<F extends IEntityFilter<F, T, ID, AO, FO>,
   T extends IEntity<T, ID>,
   ID = number,
   AO extends EntityAsObjectOptions = EntityAsObjectOptions,
-  FO = any
-  > extends IEntity<F, number /*filter ID*/, AO, FO> {
+  FO = any> extends IEntity<F, number /*filter ID*/, AO, FO> {
 
   asPodObject(): any;
+
   asFilterFn(): FilterFn<T>;
+
   isEmpty(): boolean;
+
   countNotEmptyCriteria(): number;
 }
 
-export abstract class EntityFilter<
-  F extends EntityFilter<F, T, ID, AO, FO>,
+export abstract class EntityFilter<F extends EntityFilter<F, T, ID, AO, FO>,
   T extends IEntity<T, ID>,
   ID = number,
   AO extends EntityAsObjectOptions = EntityAsObjectOptions,
-  FO = any
-  >
+  FO = any>
   extends Entity<F, number, AO, FO>
-  implements IEntityFilter<F, T, ID, AO, FO>{
+  implements IEntityFilter<F, T, ID, AO, FO> {
 
   /**
    * Clean a filter, before sending to the pod (e.g convert dates, remove internal properties, etc.)
@@ -42,31 +41,40 @@ export abstract class EntityFilter<
   }
 
   isEmpty(): boolean {
-    const json = this.asPodObject();
-    return Object.keys(json)
-      .map(key => json[key])
-      .some(value => isNotNilOrBlank(value) || isNotEmptyArray(value) || ReferentialUtils.isNotEmpty(value)) === false;
+    return this.countNotEmptyCriteria() === 0;
   }
 
   countNotEmptyCriteria(): number {
     const json = this.asPodObject();
     return Object.keys(json)
-      .map(key => json[key])
-      .filter(value => isNotNilOrBlank(value) || ReferentialUtils.isNotEmpty(value))
+      .filter(key => this.isCriteriaNotEmpty(key, json[key]))
       .length;
-  }
-
-  protected buildFilter(): FilterFn<T>[] {
-     // Can be completed by subclasses
-    return [];
   }
 
   asFilterFn(): FilterFn<T> {
     const filterFns = this.buildFilter();
-
     if (isEmptyArray(filterFns)) return undefined;
-
     return (entity) => !filterFns.find(fn => !fn(entity));
+  }
+
+  protected buildFilter(): FilterFn<T>[] {
+    // Can be completed by subclasses
+    return [];
+  }
+
+  protected isCriteriaNotEmpty(key: string, value: any): boolean {
+    // Can be overridden by subclasses
+    return value !== undefined && value !== null &&
+      (
+        // not empty string
+        (typeof value === 'string' && value.trim() !== '') ||
+        // valid number
+        (typeof value === 'number' && !isNaN(value)) ||
+        // not empty array
+        (value.length > 0) ||
+        // entity with an id
+        ReferentialUtils.isNotEmpty(value)
+      );
   }
 }
 
@@ -82,8 +90,7 @@ export abstract class EntityFilterUtils {
     const target = new filterType();
     if (EntityFilterUtils.isEntityFilter(target)) {
       target.fromObject(source);
-    }
-    else {
+    } else {
       Object.assign(target, source);
     }
     return target;
